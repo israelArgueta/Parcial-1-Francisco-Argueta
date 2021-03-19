7e0243b13400ab1ee6cac50428361dc1478d162d
israel argueta <israelargueta@mesoamericana.edu.gt>
2021-03-19 10:52:10 -0600

    Creacion de dos clases

---
+++ Parcial 1 Francisco Argueta/estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_1_Francisco_Argueta
+{
+    class estudiante
+    { 
+        string Carnet;
+        string nombre;
+        string direc;
+
+        public string Carnet1 { get => Carnet; set => Carnet = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direc { get => direc; set => direc = value; }
+    }
+}
---
+++ Parcial 1 Francisco Argueta/libros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_1_Francisco_Argueta
+{
+    class libros
+    {
+        string codigo;
+        string tiulo;
+        string autor;
+        string año;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Tiulo { get => tiulo; set => tiulo = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public string Año { get => año; set => año = value; }
+    }
+}
