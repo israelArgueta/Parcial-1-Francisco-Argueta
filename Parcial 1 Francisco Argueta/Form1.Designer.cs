64849eb8d7da438ac1bd0682e0e67a04dfc290f0
israel argueta <israelargueta@mesoamericana.edu.gt>
2021-03-19 11:34:08 -0600

    Primera parte del proyecto agregar los datos a una base de datos

--- Parcial 1 Francisco Argueta/Form1.Designer.cs
+++ Parcial 1 Francisco Argueta/Form1.Designer.cs
@@ -29,20 +29,228 @@ namespace Parcial_1_Francisco_Argueta
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Estudiante";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Carnet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Direccion";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(135, 220);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(463, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Libros";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(392, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "codigo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(396, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "titulo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(392, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Autor";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(329, 259);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(128, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Año de publicacion";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(463, 159);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(463, 206);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(463, 254);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 16;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(663, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(346, 226);
+            this.dataGridView1.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 516);
+            this.ClientSize = new System.Drawing.Size(1189, 524);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Parcial 1 Francisco Argueta/Form1.cs
+++ Parcial 1 Francisco Argueta/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,137 @@ namespace Parcial_1_Francisco_Argueta
 {
     public partial class Form1 : Form
     {
+        List<estudiante> estu = new List<estudiante>();
+        List<libros> li = new List<libros>();
+        Boolean h = false;
+        int c= 0;
         public Form1()
         {
             InitializeComponent();
         }
+        void escribirestudiante()
+        {
+            FileStream stream = new FileStream("estudiante.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter write = new StreamWriter(stream);
+            foreach (var d in estu)
+            {
+                write.WriteLine(d.Carnet1);
+                write.WriteLine(d.Nombre);
+                write.WriteLine(d.Direc);
+                
+            }
+            write.Close();
+        }
+        void leerestudiantes()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            string filename = "estudiante.txt";
+            FileStream st = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(st);
+            while (reader.Peek() > -1)
+            {
+                estudiante a = new estudiante();
+                a.Carnet1 = reader.ReadLine();
+                a.Nombre = reader.ReadLine();
+                a.Direc = reader.ReadLine();
+                estu.Add(a);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = estu;
+                dataGridView1.Refresh();
+            }
+            reader.Close();
+        }
+        void escribirlibros()
+        {
+            FileStream stream = new FileStream("libros.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter write = new StreamWriter(stream);
+            foreach (var d in li)
+            {
+                write.WriteLine(d.Codigo);
+                write.WriteLine(d.Tiulo);
+                write.WriteLine(d.Autor);
+                write.WriteLine(d.Año);
+
+            }
+            write.Close();
+        }
+        void leerlibro()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            string filename = "libros.txt";
+            FileStream st = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(st);
+            while (reader.Peek() > -1)
+            {
+                libros a = new libros();
+                a.Codigo = reader.ReadLine();
+                a.Tiulo = reader.ReadLine();
+                a.Autor = reader.ReadLine();
+                a.Año = reader.ReadLine();
+                li.Add(a);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = estu;
+                dataGridView1.Refresh();
+            }
+            reader.Close();
+        }
+        void agregar()
+        {
+            estudiante a = new estudiante();
+            a.Carnet1 = textBox1.Text;
+            a.Nombre = textBox2.Text;
+            a.Direc = textBox3.Text;
+            
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text) && !string.IsNullOrEmpty(textBox3.Text) && !string.IsNullOrEmpty(textBox4.Text) && !string.IsNullOrEmpty(textBox5.Text))
+            {
+                agregar();
+                //repetidos();
+                estudiante f = new estudiante();
+                libros s = new libros();
+                if (h)
+                {
+                    MessageBox.Show("el carnet ya esta en uso");
+                    textBox1.Clear();
+                }
+                else
+                {
+                    s.Codigo = textBox4.Text;
+                    s.Tiulo = textBox5.Text;
+                    s.Autor = textBox6.Text;
+                    s.Año = textBox7.Text;
+                    f.Carnet1 = textBox1.Text;
+                    f.Nombre = textBox2.Text;
+                    f.Direc = textBox3.Text;
+                    
+                    estu.Add(f);
+                    li.Add(s);
+                    MessageBox.Show("Se ha agregado correctamente en la base de datos");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+                    textBox4.Clear();
+                    textBox5.Clear();
+                    textBox6.Clear();
+                    textBox7.Clear();
+                    escribirlibros();
+                    escribirestudiante();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe de llenar todos los campos");
+            }
+            h = false;
+            c = 0;
+
+        }
     }
-}
+    }
+
